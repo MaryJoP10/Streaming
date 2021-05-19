708d33a165d5a5b6a8a05f2155019735ac018caa
MaryJoP10 <maryjop10@gmail.com>
2021-05-19 08:24:09 +0200

    v12

--- Streaming/pp.Designer.cs
+++ Streaming/pp.Designer.cs
@@ -93,6 +93,7 @@ namespace Streaming
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
             this.groupBox2.Controls.Add(this.cb_tipo);
             this.groupBox2.Controls.Add(this.tb_calificacion);
             this.groupBox2.Controls.Add(this.label13);
@@ -139,6 +140,7 @@ namespace Streaming
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label13.Location = new System.Drawing.Point(126, 271);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(61, 13);
@@ -149,6 +151,7 @@ namespace Streaming
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label12.Location = new System.Drawing.Point(7, 271);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(80, 13);
@@ -166,6 +169,7 @@ namespace Streaming
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label11.Location = new System.Drawing.Point(126, 199);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(105, 13);
@@ -192,6 +196,7 @@ namespace Streaming
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label10.Location = new System.Drawing.Point(6, 199);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(66, 13);
@@ -203,6 +208,7 @@ namespace Streaming
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label9.Location = new System.Drawing.Point(6, 252);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(64, 16);
@@ -214,6 +220,7 @@ namespace Streaming
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label8.Location = new System.Drawing.Point(6, 180);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(45, 16);
@@ -225,6 +232,7 @@ namespace Streaming
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label7.Location = new System.Drawing.Point(6, 109);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(51, 16);
@@ -235,6 +243,7 @@ namespace Streaming
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label6.Location = new System.Drawing.Point(6, 128);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(42, 13);
@@ -252,6 +261,7 @@ namespace Streaming
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label5.Location = new System.Drawing.Point(6, 36);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 15);
@@ -262,6 +272,7 @@ namespace Streaming
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label4.Location = new System.Drawing.Point(6, 60);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 13);
@@ -277,7 +288,8 @@ namespace Streaming
             // 
             // b_anadir
             // 
-            this.b_anadir.Location = new System.Drawing.Point(79, 323);
+            this.b_anadir.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_anadir.Location = new System.Drawing.Point(73, 323);
             this.b_anadir.Name = "b_anadir";
             this.b_anadir.Size = new System.Drawing.Size(99, 34);
             this.b_anadir.TabIndex = 3;
@@ -289,6 +301,7 @@ namespace Streaming
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label3.Location = new System.Drawing.Point(6, 16);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(138, 16);
@@ -297,6 +310,7 @@ namespace Streaming
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.Controls.Add(this.lb_info);
             this.groupBox1.Controls.Add(this.tb_vistos);
             this.groupBox1.Controls.Add(this.tb_score);
@@ -353,6 +367,7 @@ namespace Streaming
             // 
             this.label19.AutoSize = true;
             this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label19.Location = new System.Drawing.Point(6, 263);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(50, 16);
@@ -363,6 +378,7 @@ namespace Streaming
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label14.Location = new System.Drawing.Point(3, 180);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(158, 16);
@@ -371,9 +387,10 @@ namespace Streaming
             // 
             // b_agregar
             // 
-            this.b_agregar.Location = new System.Drawing.Point(62, 317);
+            this.b_agregar.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_agregar.Location = new System.Drawing.Point(33, 317);
             this.b_agregar.Name = "b_agregar";
-            this.b_agregar.Size = new System.Drawing.Size(99, 34);
+            this.b_agregar.Size = new System.Drawing.Size(152, 34);
             this.b_agregar.TabIndex = 3;
             this.b_agregar.Text = "Agregar a cuenta";
             this.b_agregar.UseVisualStyleBackColor = true;
@@ -411,6 +428,7 @@ namespace Streaming
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label2.Location = new System.Drawing.Point(6, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(155, 16);
@@ -419,6 +437,7 @@ namespace Streaming
             // 
             // groupBox3
             // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
             this.groupBox3.Controls.Add(this.b_generar);
             this.groupBox3.Controls.Add(this.b_crear);
             this.groupBox3.Controls.Add(this.tb_fecha);
@@ -436,7 +455,7 @@ namespace Streaming
             // 
             // b_generar
             // 
-            this.b_generar.Location = new System.Drawing.Point(172, 104);
+            this.b_generar.Location = new System.Drawing.Point(172, 101);
             this.b_generar.Name = "b_generar";
             this.b_generar.Size = new System.Drawing.Size(67, 21);
             this.b_generar.TabIndex = 39;
@@ -446,9 +465,10 @@ namespace Streaming
             // 
             // b_crear
             // 
-            this.b_crear.Location = new System.Drawing.Point(72, 172);
+            this.b_crear.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_crear.Location = new System.Drawing.Point(58, 170);
             this.b_crear.Name = "b_crear";
-            this.b_crear.Size = new System.Drawing.Size(99, 34);
+            this.b_crear.Size = new System.Drawing.Size(130, 34);
             this.b_crear.TabIndex = 28;
             this.b_crear.Text = "Crear cuenta";
             this.b_crear.UseVisualStyleBackColor = true;
@@ -467,6 +487,7 @@ namespace Streaming
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label18.Location = new System.Drawing.Point(6, 130);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(96, 13);
@@ -485,6 +506,7 @@ namespace Streaming
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label17.Location = new System.Drawing.Point(6, 87);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(18, 13);
@@ -502,6 +524,7 @@ namespace Streaming
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label16.Location = new System.Drawing.Point(6, 40);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(96, 13);
@@ -512,6 +535,7 @@ namespace Streaming
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label15.Location = new System.Drawing.Point(6, 16);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(98, 16);
@@ -520,6 +544,7 @@ namespace Streaming
             // 
             // groupBox4
             // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
             this.groupBox4.Controls.Add(this.cb_cuentas_info);
             this.groupBox4.Controls.Add(this.label21);
             this.groupBox4.Controls.Add(this.tb_acumulado);
@@ -545,6 +570,7 @@ namespace Streaming
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label21.Location = new System.Drawing.Point(6, 36);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(41, 13);
@@ -564,6 +590,7 @@ namespace Streaming
             // 
             this.label20.AutoSize = true;
             this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label20.Location = new System.Drawing.Point(6, 96);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(101, 13);
@@ -574,6 +601,7 @@ namespace Streaming
             // 
             this.label23.AutoSize = true;
             this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label23.Location = new System.Drawing.Point(6, 16);
             this.label23.Name = "label23";
             this.label23.Size = new System.Drawing.Size(140, 16);
@@ -583,8 +611,10 @@ namespace Streaming
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(324, 9);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(399, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(165, 63);
             this.label1.TabIndex = 35;
@@ -592,6 +622,8 @@ namespace Streaming
             // 
             // lb_cuentas
             // 
+            this.lb_cuentas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb_cuentas.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.lb_cuentas.FormattingEnabled = true;
             this.lb_cuentas.Location = new System.Drawing.Point(817, 83);
             this.lb_cuentas.Name = "lb_cuentas";
@@ -602,6 +634,7 @@ namespace Streaming
             // 
             this.label22.AutoSize = true;
             this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label22.Location = new System.Drawing.Point(6, 130);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(35, 13);
@@ -612,6 +645,7 @@ namespace Streaming
             // 
             this.label24.AutoSize = true;
             this.label24.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.label24.Location = new System.Drawing.Point(120, 130);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(82, 13);
@@ -644,6 +678,8 @@ namespace Streaming
             // 
             // lb_info
             // 
+            this.lb_info.BackColor = System.Drawing.SystemColors.Desktop;
+            this.lb_info.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.lb_info.FormattingEnabled = true;
             this.lb_info.Location = new System.Drawing.Point(9, 199);
             this.lb_info.Name = "lb_info";
@@ -654,6 +690,8 @@ namespace Streaming
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Streaming.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(926, 447);
             this.Controls.Add(this.lb_cuentas);
             this.Controls.Add(this.label1);
